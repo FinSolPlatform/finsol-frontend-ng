45063a6f492bdf5f86d5f848b07dc4c1d5f102d6
Mohamed Ali RIAHI <mariahi@localhost.localdomain>
2021-08-18 13:20:12 +0100

    Initialize Angular Project & Integrate UI

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CommunityComponent } from './community/community.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ProjectComponent } from './project/project.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'project', component: ProjectComponent },
+  { path: 'community', component: CommunityComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
