50d28ec43002de9f2f8b3a4d83a383d8e6fd0ed4
Mohamed Ali RIAHI <mariahi@localhost.localdomain>
2021-09-07 11:29:19 +0100

    Configure environment.prod.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://localhost:8080',
+  communityApiBaseUrl: 'http://localhost:8081',
+  projectApiBaseUrl: 'http://localhost:8082',
 };
