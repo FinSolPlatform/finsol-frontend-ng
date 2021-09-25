7accdd1071ead2d3a004f99210ecda835538b257
Mohamed Ali RIAHI <mariahi@localhost.localdomain>
2021-09-25 10:15:53 +0100

    delete comments from environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://user-mgt-api:8080',
+  apiBaseUrl: 'http://172.30.180.121:8080',
   communityApiBaseUrl: 'http://localhost:8081',
   projectApiBaseUrl: 'http://localhost:8082',
 };
